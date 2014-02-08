b988aabc620f48d348858f6f11f9a238cd900a7a
poemdexter <poemdexter@gmail.com>
2014-02-08 16:23:23 -0600

    campfire flickers

---
+++ Assets/scripts/CampfireFlicker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CampfireFlicker : MonoBehaviour
+{
+    public float fromLightIntensity;
+    public float toLightIntensity;
+    public float speed;
+    // Update is called once per frame
+    void Start()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash("from", fromLightIntensity, "to", toLightIntensity, "speed", speed, "loopType", "pingPong", "onUpdate", "ChangeIntensity"));
+    }
+    
+    void ChangeIntensity(float value)
+    {
+        light.intensity = value;
+    }
+}
